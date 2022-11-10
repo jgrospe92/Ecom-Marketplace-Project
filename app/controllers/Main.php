931c290806a3d63290b048bd3a26dc17ece5fbdb
Jeffrey <77903214+jgrospe92@users.noreply.github.com>
2022-11-10 10:41:26 -0500

    added base view files and controllers

---
+++ app/controllers/Main.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace app\core\Controllers {
+
+    class Main extends \app\core\Controller{
+
+        public function index(){
+            echo "main";
+        }
+    }
+}
--- app/core/autoload.php
+++ app/core/autoload.php
@@ -1,7 +1,9 @@
 <?php
 //this is 75% of a PSR-4 autoloader
 spl_autoload_register(
-	function ($class_name){
-		require_once($class_name . ".php");
-	}
+
+	// comment this if you're on mac
+	// function ($class_name){
+	// 	require_once($class_name . ".php");
+	// }
 );
