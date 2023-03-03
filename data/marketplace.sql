af79bc1e84522bdf0e3a48e69e9b3ef26c943790
Jeffrey <jeffreygrospe@yahoo.com>
2023-03-03 15:58:09 -0500

    fix create db

--- data/marketplace.sql
+++ data/marketplace.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `marketplace`
 --
-
+CREATE DATABASE `marketplace`;
+Use `marketplace`;
 -- --------------------------------------------------------
 
 --
